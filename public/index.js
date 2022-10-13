c1d905bcdd1024165e01c7399181fc55fbb5de31
Amanda <amandarb43@gmail.com>
2022-10-12 19:05:19 -0700

    initial

--- public/index.js
+++ public/index.js
@@ -1,4 +1,13 @@
 async function main() {
+    let url = 'https://api.twelvedata.com/time-series&apikey=cc672c50cb8e40078af70f7e07348a08';
+    let stockData = ["GME","MSFT","DIS","BNTX"]
+   let result = await fetch(url + stockData)
+    .then(()=>{
+        console.log(result)
+    })
+    .catch(error=>{
+        console.log(error);
+    })
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
@@ -7,3 +16,4 @@ async function main() {
 }
 
 main()
+//Sorry for this I am still trying to figure this code out. This was all I could do so far.
